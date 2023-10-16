279c58d48384f0098622d212569bbd2c0a5f2dd8
Kayra Ulusoy <115214533+Tnhem@users.noreply.github.com>
2023-10-16 15:56:48 +0300

    Update HelloWorld.java
    for bim207

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,8 +2,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println(10.6 * 2);
-        System.out.println((110 + 2 * 8) / (8 * 7));
-        System.out.println(5 * 5 * 3.1415);
     }
 }
